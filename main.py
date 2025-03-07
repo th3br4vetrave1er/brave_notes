c8dde1b8d04dfac29ddca32a1b92fa574858e205
th3br4vetrave1er <tazdingodan@gmail.com>
2025-03-07 23:08:37 +0200

    replaced buttons, but still kinda stiff. tkinter is tough, need to practice it more!

--- main.py
+++ main.py
@@ -18,22 +18,37 @@ class SimpleNotepad:
         self.button_frame: Frame = Frame(self.root)
         self.button_frame.pack()
 
-        # Save button
-        self.save_button: Button = Button(self.button_frame, text='Save', command=self.save_file)
-        self.save_button.pack(side=tk.LEFT)
+        # Saveas button
+        self.saveas_button: Button = Button(self.button_frame, text='Save as...', command=self.saveas_file)
+        self.saveas_button.pack(side=tk.LEFT)
         # Load button
         self.load_button: Button = Button(self.button_frame, text='Load', command=self.load_file)
         self.load_button.pack(side=tk.LEFT)
+        # Save button
+        self.save_button: Button = Button(self.button_frame, text='Save', command=self.save_file)
+        self.save_button.pack(side=tk.LEFT)
 
-    def save_file(self) -> None:
+        # Variable for current path to file
+        self.current_file: str = None
+
+    def saveas_file(self) -> None:
         file_path: str = filedialog.asksaveasfilename(defaultextension='.txt',
                                                       filetypes=[('Text files', '*.txt')])
         with open(file_path, 'w') as file:
             file.write(self.text_area.get(1.0, tk.END))
+        self.current_file = file_path
 
         print(
             f'File saved to {file_path}.'
         )
+
+    def save_file(self) -> None:
+        if self.current_file:
+            with open(self.current_file, 'w', encoding='utf-8') as file:
+                file.write(self.text_area.get(1.0, tk.END))
+                print(
+                    f'File saved to {self.current_file}'
+                )
 
     def load_file(self) -> None:
         file_path: str = filedialog.askopenfilename(defaultextension='.txt',
@@ -57,4 +72,3 @@ def main() -> None:
 if __name__ == '__main__':
     main()
 
-# TODO Add save button, replace save button title with save as title
